509256f37301b21bbfede669e7056dcec664f7c1
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-09-05 05:11:03 +0300

    создано соединение с базой данных

---
+++ src/main/java/org/example/constant/ConstantApp.java
@@ -0,0 +1,16 @@
+package org.example.constant;
+
+public interface ConstantApp {
+    String DATABASE_PROPERTIES_FILE_PATH = "application.yml";
+
+    String DATABASE_DRIVER_KEY = "driver-class-name";
+    String DATABASE_URL_KEY = "url";
+    String DATABASE_USER_KEY = "username";
+    String DATABASE_PASSWORD_KEY = "password";
+
+
+
+    String DDL_INITIALIZATION_SCRIPT_PATH = "D:\\AllbackProgect\\23.08.23_Clevertec_test\\Clevertec_test\\src\\main\\resources\\database\\DDL_myBase.sql";
+    String DML_INITIALIZATION_SCRIPT_PATH = "D:\\AllbackProgect\\23.08.23_Clevertec_test\\Clevertec_test\\src\\main\\resources\\database\\DML_myBase.sql";
+    String DDL_INITIALIZATION_DROP_PATH = "D:\\AllbackProgect\\23.08.23_Clevertec_test\\Clevertec_test\\src\\main\\resources\\database\\Drop_myBase.sql";
+}
